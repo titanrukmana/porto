c3315261200b3b911d7a9af7294656cb5d3675ad
unknown <titan170300@gmail.com>
2022-11-09 00:40:22 +0700

    tab name and icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Grid, Container, ThemeProvider } from "@mui/material";
 import Header from "../components/molecules/header";
 import Page1 from "../components/molecules/page1";
@@ -9,6 +8,13 @@ import { theme } from "../styles/theme";
 export default function Home() {
 	return (
 		<>
+			<div>
+				<Head>
+					<title>Titan Rukmana</title>
+					<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+					<link rel="shortcut icon" href="/icon/favicon.ico" />
+				</Head>
+			</div>
 			<ThemeProvider theme={theme}>
 				<Header />
 				<Page1 />
